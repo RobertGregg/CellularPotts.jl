8a6535c539194970d2a49ee65189345a3c019a66
RobertGregg <robwgregg@gmail.com>
2021-11-29 16:43:53 -0800

    Added Spatial Dimension to LV eqs

--- src/jumpDiffEq.jl
+++ src/jumpDiffEq.jl
@@ -47,3 +47,67 @@ sol = solve(jump_prob, SSAStepper())
 
 plot(sol)
 plot(sol, vars=(1,2))
+
+######################################################
+# Lotka-Volterra Equations (1-D movement on lattice)
+######################################################
+
+# Number of lattice points
+N = 64
+
+#Parameters are time and rate constant, Variables are amount of predator/prey at each point
+@parameters t, h, α, β̄, δ, γ
+@variables X[1:N](t) Y[1:N](t)
+
+#Rate to jump between lattice points
+rate = 1/h
+
+rxns = [] 
+
+for i in 1:N
+
+    if i ≠ 1
+        push!(rxns, Reaction(rate, [X[i]], [X[i-1]]))
+        push!(rxns, Reaction(rate, [Y[i]], [Y[i-1]]))
+    end
+
+    if i ≠ N
+        push!(rxns, Reaction(rate, [X[i]], [X[i+1]]))
+        push!(rxns, Reaction(rate, [Y[i]], [Y[i+1]]))
+    end
+
+    push!(rxns, Reaction(α, [X[i]], [X[i]], [1], [2]))
+    push!(rxns, Reaction(β̄, [X[i],Y[i]], [Y[i]]))
+    push!(rxns, Reaction(δ, [X[i],Y[i]], [Y[i]], [1,1], [2]))
+    push!(rxns, Reaction(γ, [Y[i]], nothing))
+
+end
+
+
+@named LV_spatial = ReactionSystem(rxns, t, [X;Y], [h, α, β̄, δ, γ])
+
+
+
+p     = (1/N, 2.0, 0.02, 0.02, 1.06) #β̄ = β-δ = 0.04-0.02
+u₀    = zeros(Int64, 2*N)
+tspan = (0.0,20.0)
+
+u₀[1] = 100 #100 prey in first lattice
+u₀[end] = 100 #100 predators in last lattice
+
+
+prob  = DiscreteProblem(LV_spatial, u₀, tspan, p)
+jump_prob = JumpProblem(LV_spatial, prob, RSSA(),save_positions=(false,false))
+
+sol = solve(jump_prob, SSAStepper(), saveat=0.01)
+
+plot(sol, legend=nothing)
+
+
+anim = @animate for currState ∈ sol.u
+    bar(currState[1:N], alpha=0.5, labels="prey")
+    bar!(currState[N+1:end],alpha=0.5, labels="predator")
+    ylims!(0,400)
+end
+
+gif(anim, "anim.gif", fps = 60)
