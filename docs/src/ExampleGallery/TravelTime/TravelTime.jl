529d0860e7eeeb0958c9b860ee83eb5ec95c93b8
RobertGregg <robwgregg@gmail.com>
2023-12-06 15:32:00 -0800

    Typos in TravelTime

--- docs/src/ExampleGallery/TravelTime/TravelTime.jl
+++ docs/src/ExampleGallery/TravelTime/TravelTime.jl
@@ -8,7 +8,7 @@
 #   msd(\tau) = <\Delta r(\tau)^2> = <(r(t+\tau) - r(t))^2>
 # ```
 
-# Here r(t) represents the current position of the cell and the brackets indicate an average over time. If cell is moving randomly, this function will be linear. Cells with directional motion will deflect the curve upward. Let's see if we can replicate these theories. 
+# Here r(t) represents the current position of the cell and the brackets indicate an average over time. If a cell is moving randomly, this function will be linear. Cells with directional motion will deflect the curve upward. Let's see if we can replicate these theoretic results. 
 
 # ## Create a model containing a cell with and without directional motion.
 
@@ -19,7 +19,7 @@ using Random, Statistics
 Random.seed!(314);
 
 #Models have same space and cell initializations
-space = CellSpace(200,200; periodic=true, diagonal=true)
+space = CellSpace(200,200)
 
 #Center the moving cell to avoid edges and put stationary cell off into a corner
 positions = [(30,30),(100,100)]
@@ -57,8 +57,10 @@ function meanPosition(space, n)
     totalPixels = 0
     avePos = zeros(2) #x and y positions
 
+    #Loop over all points in space
     for i in axes(space,1)
         for j in axes(space,2)
+            #Save positions that match cell ID
             if space[i,j] == n
                 avePos[1] += i
                 avePos[2] += j
@@ -90,6 +92,7 @@ plot!(p1, trajectoryDirected[1,:], trajectoryDirected[2,:])
 
 MeanSqDis(cpm, τ, id) = mean([sum(abs2, meanPosition(cpm(i+τ).nodeIDs, id) - meanPosition(cpm(i).nodeIDs, id)) for i in 1:τ:cpm.step.counter-τ])
 
+# Here we choose a lag time of 50 steps (arbitrary)
 
 scatter([MeanSqDis(cpm, τ, 1) for τ in 1:50],
     title = "Random Motion",
@@ -97,6 +100,8 @@ scatter([MeanSqDis(cpm, τ, 1) for τ in 1:50],
     ylabel = "Mean Squared Displacement",
     legend = false,
     framestyle = :box)
+
+# And plotting the directed motion
 
 scatter([MeanSqDis(cpm, τ, 2) for τ in 1:50],
     title = "Directed Motion",
