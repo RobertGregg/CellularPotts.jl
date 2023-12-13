797eff40183334e8d80aef2c684e1f4831affb68
Documenter.jl <documenter@juliadocs.github.io>
2023-12-13 22:07:50 +0000

    build based on 91de62e

--- dev/GenerateExamples.jl
+++ dev/GenerateExamples.jl
@@ -29,9 +29,9 @@ end
 #Loop through all the examples, execute them, save markdown file
 
 examplesToUpdate = ["OnPatrol.jl"]
-for (root, dirs, files) in walkdir(joinpath(@__DIR__,"ExampleGallery"))
+for (root, dirs, files) in walkdir("./docs/src/ExampleGallery")
     for file in files
-        if endswith(file,".jl") && file ∈ examplesToUpdate
+        if endswith(file,".jl") #&& file ∈ examplesToUpdate
             Literate.markdown(joinpath(root, file), root; execute=true, postprocess=str->gifReplace(str,root,file))
         end
     end
