d06bebd21b1a89d697d40d23a099d1ffcb3e7f2b
Robert Gregg <robwgregg@gmail.com>
2024-08-05 22:51:16 -0700

    added migration

--- src/VisualUpdate.jl
+++ src/VisualUpdate.jl
@@ -2,60 +2,69 @@ using CellularPotts
 using GLMakie
 using Colors
 
-
 mutable struct Visual
     figure::Figure
     axis::Axis
-    hm::Heatmap{Tuple{Vector{Float32}, Vector{Float32}, Matrix{Float32}}}
-    ls::Lines{Tuple{Vector{Point{2, Float32}}}}
     sizeSpace::Tuple{Int64,Int64}
     cellColors::Vector{RGBA{Float64}}
     migrationColors::Vector{RGBA{Float64}}
     borders::Vector{Point{2, Float32}}
-
-    function Visual(cpm::CellPotts)
-       
-        figure  = Figure(size = (600, 600))
-    
-        axis = Axis(figure[1, 1], aspect = DataAspect())
-        hidedecorations!(axis)
-    
-        cellColors = [
-            RGBA(221/255, 221/255, 221/255, 1.0) #off-white
-            RGBA(119/255, 170/255, 221/255, 1.0) #light blue
-            RGBA(238/255, 221/255, 136/255, 1.0) #pale yellow
-            RGBA(68/255, 187/255, 153/255, 1.0) #mint green
-            RGBA(238/255, 136/255, 102/255, 1.0) #peach
-            RGBA(255/255, 170/255, 187/255, 1.0) #pink
-            ]
-
-        cellColors = cellColors[1:(countcelltypes(cpm)+1)]
-        migrationColors = range(RGBA(255,255,255,0.0), RGBA(60,4,104,0.6))
-    
-        sizeSpace = size(cpm.space)
-        hm = heatmap!(axis, cpm.space.nodeIDs; colormap=Makie.Categorical(cellColors))
-
-        borders = Point2f.(fill(NaN, 3*prod(sizeSpace)),fill(NaN, 3*prod(sizeSpace)))
-        updateBorders!(borders, cpm.space)
-
-        ls = lines!(axis, borders, color=:black)
-
-    
-        visual =  new(
-            figure,
-            axis,
-            hm,
-            ls,
-            sizeSpace,
-            cellColors,
-            migrationColors,
-            borders
-        )
-
-
-        return visual
-    end
+    migrationIndex::Int64
 end
+
+
+function Visual(cpm::CellPotts)
+       
+    figure  = Figure(size = (600, 600))
+
+    axis = Axis(figure[1, 1], aspect = DataAspect())
+    hidedecorations!(axis)
+
+    cellColors = [
+        RGBA(221/255, 221/255, 221/255, 1.0) #off-white
+        RGBA(119/255, 170/255, 221/255, 1.0) #light blue
+        RGBA(238/255, 221/255, 136/255, 1.0) #pale yellow
+        RGBA(68/255, 187/255, 153/255, 1.0) #mint green
+        RGBA(238/255, 136/255, 102/255, 1.0) #peach
+        RGBA(255/255, 170/255, 187/255, 1.0) #pink
+        ]
+
+    cellColors = cellColors[1:(countcelltypes(cpm)+1)]
+    migrationColors = range(RGBA(1,1,1,0.0), RGBA(60/255, 4/255, 104/255, 0.6))
+
+    #Cell IDs
+    sizeSpace = size(cpm.space)
+    heatmap!(axis, cpm.space.nodeIDs; colormap=Makie.Categorical(cellColors))
+
+    #Cell Borders
+    borders = Point2f.(fill(NaN, 3*prod(sizeSpace)),fill(NaN, 3*prod(sizeSpace)))
+    updateBorders!(borders, cpm.space)
+
+    lines!(axis, borders, color=:black)
+
+    #Cell Migration
+    migrationIndex = findfirst(x->x isa MigrationPenalty, cpm.penalties)
+
+    if !isnothing(migrationIndex)
+        heatmap!(axis, cpm.penalties[migrationIndex].nodeMemory; colormap=migrationColors)
+    else
+        migrationIndex = 0
+    end
+
+    return Visual(
+        figure,
+        axis,
+        sizeSpace,
+        cellColors,
+        migrationColors,
+        borders,
+        migrationIndex
+    )
+
+end
+
+Base.getindex(visual::Visual,i) = visual.axis.scene.plots[i]
+Base.length(visual::Visual) = length(visual.axis.scene.plots)
 
 updateBorders!(visual::Visual,space::CellSpace) = updateBorders!(visual.borders, space)
 
@@ -98,29 +107,33 @@ function updateBorders!(borders,space::CellSpace)
     return nothing
 end
 
+space = CellSpace(100,100; diagonal=true)
+initialCellState = CellState(:Epithelial, 500, 1; positions = size(space) .÷ 2);
 
 
-space = CellSpace(50,50; periodic=false, diagonal=true)
-initialCellState = CellState([:A,:B,:C,:D], [425,425,425,425], [1,1,1,1]; positions = [(10,10),(40,40),(10,40),(40,10)])
 penalties = [
-    AdhesionPenalty([0 20 20 20 20;
-                     20 0 20 20 20;
-                     20 20 0 20 20;
-                     20 20 20 0 20;
-                     20 20 20 20 0]),
-    VolumePenalty([5,5,5,5])
+    AdhesionPenalty([0 30;
+                    30 30]),
+    VolumePenalty([5]),
+    PerimeterPenalty([0,10]),
+    MigrationPenalty(50, [50], size(space))
     ]
+
 
 cpm = CellPotts(space, initialCellState, penalties)
 
 visual = Visual(cpm)
 
-record(visual.figure, "test.gif", 1:300) do i
-    ModelStep!(cpm)
+record(visual.figure, "test.gif", 1:200) do i
 
+    ModelStep!(cpm)
 
     updateBorders!(visual, cpm.space)
     
-    visual.hm[1] = cpm.space.nodeIDs
-    visual.ls[1] = visual.borders
+    visual[1][1] = cpm.space.nodeIDs
+    visual[2][1] = visual.borders
+
+    if visual.migrationIndex > 0
+        visual[3][1] = cpm.penalties[visual.migrationIndex].nodeMemory
+    end
 end
